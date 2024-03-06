91da2b79d12e6fd78f3d82a57318f831da95f1d6
Nontrust <deed1515@naver.com>
2024-03-06 18:35:51 +0900

    :memo: [memo] express 말고 다른 node 프레임워크도 되는듯

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { PORT } from '../constant';
 
+/** when using default(express) context */
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(PORT);
 }
+
 bootstrap();
+
+/** when using Fastify context -> 아직 11%정도만 기능 구현... */
+// const bootstrapFastify = async () => {
+//   const app = await NestFactory.create<NestFastifyApplication>(
+//     AppModule,
+//     new FastifyAdapter(),
+//   );
+//   await app.listen(PORT);
+//   // (3000, '0.0.0.0');
+// };
+// bootstrapFastify();
