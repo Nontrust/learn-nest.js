07241c4b24542958f00bde710dda2a299d02ea04
Nontrust <deed1515@naver.com>
2024-04-03 15:37:35 +0900

    :white_check_mark: [learn] 네스트 어노테이션 기초 확인

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,21 +1,27 @@
 import {
-  Controller,
-  Get,
-  Post,
   Body,
-  Patch,
-  Param,
+  Controller,
   Delete,
+  Get,
+  Header,
+  HttpCode,
+  Param,
+  Patch,
+  Post,
+  Redirect,
+  Req,
 } from '@nestjs/common';
 import { CatsService } from './cats.service';
 import { CreateCatDto } from './dto/create-cat.dto';
 import { UpdateCatDto } from './dto/update-cat.dto';
+import { Request } from 'express';
 
 @Controller('cats')
 export class CatsController {
   constructor(private readonly catsService: CatsService) {}
 
   @Post()
+  @HttpCode(201)
   create(@Body() createCatDto: CreateCatDto) {
     return this.catsService.create(createCatDto);
   }
@@ -25,18 +31,36 @@ export class CatsController {
     return this.catsService.findAll();
   }
 
-  @Get(':id')
+  @Get('say/:id')
   findOne(@Param('id') id: string) {
     return this.catsService.findOne(+id);
   }
 
-  @Patch(':id')
+  @Patch('say/:id')
   update(@Param('id') id: string, @Body() updateCatDto: UpdateCatDto) {
     return this.catsService.update(+id, updateCatDto);
   }
 
-  @Delete(':id')
+  @Delete('say/:id')
   remove(@Param('id') id: string) {
     return this.catsService.remove(+id);
+  }
+
+  @Post('auth')
+  @Header('Cache-Control', 'X-authorization')
+  findAllRequest(@Req() request: Request) {
+    return request;
+  }
+
+  @Get('redirect')
+  @Redirect('moves', 302)
+  redirectToMovingCats() {
+    // return req;
+  }
+
+  @Get('moves')
+  movingCats(@Req() req: Request) {
+    console.log('req', req);
+    return 'moving!';
   }
 }
