34f59293917efeb3a97da1dc7b202df84541bf94
Nontrust <deed1515@naver.com>
2024-05-03 16:56:08 +0900

    :truck: [log]  패키지 변경

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
 import { BoardModule } from './board/board.module';
-import { LoggerModule } from './logger/logger.module';
+import { LoggerModule } from './config/core/logger/logger.module';
 
 @Module({
   imports: [CatsModule, BoardModule, LoggerModule],
--- src/board/board.module.ts
+++ src/board/board.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { BoardController } from './board.controller';
 import { BoardService } from './board.service';
-import { LoggerModule } from '../logger/logger.module';
+import { LoggerModule } from '../config/core/logger/logger.module';
 
 @Module({
   imports: [BoardModule, LoggerModule],
