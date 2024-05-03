00d4ecd073f935c227ff4a2dacd42f7f5b72213b
Nontrust <deed1515@naver.com>
2024-05-03 14:20:23 +0900

    :heavy_plus_sign: [logger-module] 로거 모듈 추가, 서비스 별 import

--- src/board/board.module.ts
+++ src/board/board.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { BoardController } from './board.controller';
 import { BoardService } from './board.service';
+import { LoggerModule } from '../logger/logger.module';
 
 @Module({
-  imports: [BoardModule],
+  imports: [BoardModule, LoggerModule],
   controllers: [BoardController],
   providers: [BoardService],
 })
-export class BoardModule {
-
-}
+export class BoardModule {}
