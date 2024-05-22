fd27b51bab1b5934e6e6f366dbf130ba4b0207cb
Nontrust <deed1515@naver.com>
2024-05-22 14:30:43 +0900

    :card_file_box: [typeorm] entity 경로 설정 (cwd + dist)  - option 로깅 추가

--- src/config/core/config.typeorm.ts
+++ src/config/core/config.typeorm.ts
@@ -1,22 +1,27 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { YamlService } from '@app/src/config/core/yaml/yaml.service';
 import { DatabaseType } from '@app/src/config/core/database/database.type';
+import { LoggerService } from '@app/src/config/core/logger/logger.service';
 
 const getOrmConfig = (
-  yamlService: YamlService,
   databaseType: DatabaseType,
+  yamlService: YamlService,
+  loggerService: LoggerService,
 ): TypeOrmModuleOptions => {
   const PREFIX = `db.${databaseType}`;
-  return {
+  const options = {
     type: databaseType,
     host: yamlService.get(`${PREFIX}.host`),
     port: parseInt(yamlService.get(`${PREFIX}.port`)),
     username: yamlService.get(`${PREFIX}.username`),
     password: yamlService.get(`${PREFIX}.password`).toString(),
     database: yamlService.get(`${PREFIX}.database`),
-    synchronize: yamlService.get(`${PREFIX}.synchronize`) === 'true', // 개발 환경에서만 true로 설정하세요. 운영 환경에서는 false로 설정하세요.
-    entities: [__dirname + '/**/*.entity{.ts,.js}'],
+    synchronize: yamlService.get(`${PREFIX}.synchronize`) === true, // 개발 환경에서만 true로 설정하세요. 운영 환경에서는 false로 설정하세요.
+    entities: [process.cwd() + '/dist/**/*.entity{.ts,.js}'],
   };
+
+  loggerService.log('TypeOrm option is', options);
+  return options;
 };
 
 export default getOrmConfig;
--- src/config/core/database/database.module.ts
+++ src/config/core/database/database.module.ts
@@ -3,17 +3,19 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import getOrmConfig from '../config.typeorm';
 import { YamlService } from '@app/src/config/core/yaml/yaml.service';
 import { ConfigModule } from '@nestjs/config';
+import { LoggerService } from '@app/src/config/core/logger/logger.service';
+import { LoggerModule } from '@app/src/config/core/logger/logger.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [YamlService],
-      useFactory: (yamlService: YamlService) =>
-        getOrmConfig(yamlService, 'mysql'),
+      imports: [ConfigModule, LoggerModule],
+      inject: [YamlService, LoggerService],
+      useFactory: (yamlService: YamlService, loggerService: LoggerService) =>
+        getOrmConfig('mysql', yamlService, loggerService),
     }),
   ],
-  providers: [YamlService],
+  providers: [YamlService, LoggerService],
   exports: [TypeOrmModule],
 })
 export class DatabaseModule {}
